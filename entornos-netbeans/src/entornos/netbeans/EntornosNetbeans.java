138f0da3987037ed6b37b0278161119779428212
Menosuno02 (Alejandro López) <mainusone@gmail.com>
2022-03-30 10:47:33 +0200

    Modified main (unique numbers)

--- entornos-netbeans/src/entornos/netbeans/EntornosNetbeans.java
+++ entornos-netbeans/src/entornos/netbeans/EntornosNetbeans.java
@@ -14,10 +14,23 @@ public class EntornosNetbeans {
      */
     public static void main(String[] args) {
         int[] num = new int[10];
+        int numIntroducido;
+        boolean introducido;
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i <= num.length - 1; i++) {
             System.out.println("Introduce número");
             num[i] = sc.nextInt();
+            do {
+                introducido = false;
+                System.out.println("Introduce número " + (i + 1));
+                numIntroducido = sc.nextInt();
+                for (int j = 0; j <= i - 1; j++) {
+                    if (numIntroducido == num[j]) {
+                        introducido = true;
+                    }
+                }
+            } while (introducido);
+            num[i] = numIntroducido;
         }
         Arrays.sort(num);
     }
